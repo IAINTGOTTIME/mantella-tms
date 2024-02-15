a9ccd0870f344fbb8290169704ff634bbb048d3b
lardira <danilkefimov@gmail.com>
2024-02-15 21:54:34 +0300

    added type hints

--- src/backend/api/v1/controllers/test_cases.py
+++ src/backend/api/v1/controllers/test_cases.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, HTTPException
 
 from entities.test_cases import TestCase
@@ -12,12 +14,12 @@ service = TestCaseService()
 
 
 @test_cases_router.get("/")
-def get_test_cases():
+def get_test_cases() -> List[TestCase]:
     return service.get_all()
 
 
 @test_cases_router.get("/{id}")
-def get_one_test_case(id: str):
+def get_one_test_case(id: str) -> TestCase:
     one = service.get_one(id)
     if not one:
         raise HTTPException(detail=f"test case with id {id} not found",
@@ -26,13 +28,13 @@ def get_one_test_case(id: str):
 
 
 @test_cases_router.post("/")
-def create_test_case(new_item: TestCase):
+def create_test_case(new_item: TestCase) -> TestCase:
     new_one = service.create(new_item)
     return new_one
 
 
 @test_cases_router.put("/{id}")
-def update_test_case(id: str, new_item: TestCase):
+def update_test_case(id: str, new_item: TestCase) -> TestCase:
     new_one = service.update(id, new_item)
     if not new_one:
         raise HTTPException(detail=f"test case with id {id} not found",
@@ -41,7 +43,7 @@ def update_test_case(id: str, new_item: TestCase):
 
 
 @test_cases_router.delete("/{id}")
-def delete_test_case(id: str):
+def delete_test_case(id: str) -> str:
     deleted_id = service.delete(id)
     if not deleted_id:
         raise HTTPException(detail=f"test case with id {id} not found",

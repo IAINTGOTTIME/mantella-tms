fb21b2031d1a452dd01fff4e7bf9b1f5f1bff5fc
notime <vlad-kotov-01-01@mail.ru>
2024-02-16 18:13:08 +0300

    check lists router

--- src/backend/api/v1/controllers/check_lists.py
+++ src/backend/api/v1/controllers/check_lists.py
@@ -2,42 +2,42 @@ from typing import List
 
 from fastapi import APIRouter, HTTPException
 
-from entities.check_lists import CheckLists, CheckListsItems
+from entities.check_lists import CheckList, CheckListsItem
 
 check_lists_router = APIRouter(
     tags=["check-lists"],
     prefix="/check-lists"
 )
 
-MOCK_CHECK_LISTS: List[CheckLists] = [
-    CheckLists(
+MOCK_CHECK_LISTS: List[CheckList] = [
+    CheckList(
         id="1",
-        items=[CheckListsItems(id=1, description="dasdasdasd321312312312"),
-               CheckListsItems(id=2, description="dasdasdasd2313132313"),
-               CheckListsItems(id=3, description="dasdasdasdASAADADADS"),
-               CheckListsItems(id=4, description="dasdasdadfg"),
-               CheckListsItems(id=5, description="dasdasd321"),
+        items=[CheckListsItem(id=1, description="dasdasdasd321312312312"),
+               CheckListsItem(id=2, description="dasdasdasd2313132313"),
+               CheckListsItem(id=3, description="dasdasdasdASAADADADS"),
+               CheckListsItem(id=4, description="dasdasdadfg"),
+               CheckListsItem(id=5, description="dasdasd321"),
                ]
     ),
-    CheckLists(
+    CheckList(
         id="2",
-        items=[CheckListsItems(id=1, description="dasdasdasd321312312312"),
+        items=[CheckListsItem(id=1, description="dasdasdasd321312312312"),
 
                ]
     ),
-    CheckLists(
+    CheckList(
         id="3",
-        items=[CheckListsItems(id=1, description="dasdasdasd321312312312"),
-               CheckListsItems(id=4, description="dasdasdadfg"),
-               CheckListsItems(id=5, description="dasdasd321"),
+        items=[CheckListsItem(id=1, description="dasdasdasd321312312312"),
+               CheckListsItem(id=4, description="dasdasdadfg"),
+               CheckListsItem(id=5, description="dasdasd321"),
                ]
     ),
 
-    CheckLists(
+    CheckList(
         id="4",
-        items=[CheckListsItems(id=1, description="dasdasdasd321312312312"),
-               CheckListsItems(id=2, description="dasdasdasd2313132313"),
-               CheckListsItems(id=3, description="dasdasdasdASAADADADS"),
+        items=[CheckListsItem(id=1, description="dasdasdasd321312312312"),
+               CheckListsItem(id=2, description="dasdasdasd2313132313"),
+               CheckListsItem(id=3, description="dasdasdasdASAADADADS"),
 
                ]
     )
@@ -58,13 +58,13 @@ def get_one_check_list(id: str):
 
 
 @check_lists_router.post("/")
-def create_check_list(new_item: CheckLists):
+def create_check_list(new_item: CheckList):
     MOCK_CHECK_LISTS.append(new_item)
     return new_item
 
 
 @check_lists_router.put("/{id}")
-def update_check_list(id: str, new_item: CheckLists):
+def update_check_list(id: str, new_item: CheckList):
     for i, check_lists in enumerate(MOCK_CHECK_LISTS):
         if check_lists.id == id:
             MOCK_CHECK_LISTS[i] = new_item

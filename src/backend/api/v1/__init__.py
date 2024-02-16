d51479d6e436a7e3cd6ad95bb87b7e1724b97ff6
lardira <78199923+lardira@users.noreply.github.com>
2024-02-16 18:03:28 +0300

    Mock Test Case API (#25)
    * added service layer
    
    * added type hints
    
    * removed unneded controller example
    
    * fix removed router

--- src/backend/api/v1/__init__.py
+++ src/backend/api/v1/__init__.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 
 from .controllers.test_cases import test_cases_router
-from .controllers.users import users_router
 
 router = APIRouter(
     prefix='/v1'
 )
 
-router.include_router(users_router)
 router.include_router(test_cases_router)

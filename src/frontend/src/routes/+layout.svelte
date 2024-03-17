f2537b8e6117cca533e38105d2db1299900d9afd
lardira <danilkefimov@gmail.com>
2024-03-17 12:23:25 +0300

    theme dark mode switching

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -1,1 +1,23 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+  import '../app.pcss';
+  import { AppShell } from '@skeletonlabs/skeleton';
+  import { LightSwitch } from '@skeletonlabs/skeleton';
+  import { autoModeWatcher } from '@skeletonlabs/skeleton';
+</script>
+
+<svelte:head>{@html '<script>(' + autoModeWatcher.toString() + ')();</script>'}</svelte:head>
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4" >
+  <svelte:fragment slot="sidebarLeft" >
+      <nav class="list-nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+        </ul>
+      </nav>
+      <LightSwitch class='absolute bottom-2 left-2' />
+
+  </svelte:fragment>
+</AppShell>
+
+<style>
+</style>
--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto p-8 space-y-8">
+  <h1 class="h1">Hello Skeleton</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+</div>
